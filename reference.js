7b13ff196302e40372a8270a323c2636ba74ed89
Prashanth <prashanthbabu.thippeswamy@aig.com>
2020-01-07 17:13:14 +0530

    Updated Show

---
+++ reference.js
@@ -0,0 +1,14 @@
+// const Post = mongoose.model("Campground", campgroundSchema);
+
+// app.get('/', async (req, res) => {
+//  let post = await Post.create({title: 'Test', description: 'This is a test for Dashboard third instance'});
+//  res.send(post);
+// });
+//to start mongo with terminal :
+//http://www.dba86.com/docs/mongo/2.4/tutorial/getting-started-with-the-mongo-shell.html;
+
+//Mongo DB instructions
+    // show dbs : to show list of Database
+    // use BootCamp - to Switch to Database
+    // show collections - to show the list of collections 
+    // db.campgrounds.find() - to list number of objects 
